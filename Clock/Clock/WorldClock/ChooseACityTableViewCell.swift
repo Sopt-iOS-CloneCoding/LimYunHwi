9e6c18ba6ae88708768f674d8cced6ec5cfaef53
LimYunHwi <88yhtserof@naver.com>
2022-04-25 14:16:20 +0900

    #1 [Feat] 세계 시계 : 도시 선택 화면 UI 구성
    1. 도시 선택 화면 UI 구성
    2. 도시 선택 화면 TableViewCell UI 구성
    
    + DataModel 폴더링

---
+++ Clock/Clock/WorldClock/ChooseACityTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ChooseACityTableViewCell.swift
+//  Clock
+//
+//  Created by 임윤휘 on 2022/04/25.
+//
+
+import UIKit
+
+class ChooseACityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
---
+++ Clock/Clock/WorldClock/ChooseACityViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChooseACityViewController.swift
+//  Clock
+//
+//  Created by 임윤휘 on 2022/04/25.
+//
+
+import UIKit
+
+class ChooseACityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
