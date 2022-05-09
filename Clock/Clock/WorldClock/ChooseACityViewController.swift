f1656d68df688fdf541b3344744be543eb55a747
LimYunHwi <88yhtserof@naver.com>
2022-05-09 21:47:54 +0900

    #1 [Feat] 세계 시계 : 도시 검색
    1.도시 명 검색 시 해당 알파벳을 가진 모든 도시 보이기
    2. 검색어가 없을 때 기본 도시 리스트 보이기
    
    + 검색어 중간 수정 불가능. 기능 수정 필요.

--- Clock/Clock/WorldClock/ChooseACityViewController.swift
+++ Clock/Clock/WorldClock/ChooseACityViewController.swift
@@ -11,10 +11,12 @@ class ChooseACityViewController: UIViewController {
     @IBOutlet weak var cityTableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
-    var cityColocks: [CityClock] = []
+    var mutableCityColocks: [CityClock] = []
     var sectionTitles: [String] = []
     var cityColockDict = [String : [CityClock]]()
     var headerTitles: [String] = []
+    var isSearching: Bool = false
+    var cityClocks: [CityClock] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +24,9 @@ class ChooseACityViewController: UIViewController {
         cityTableView.dataSource = self
         searchBar.delegate = self
         
-        cityColocks = getCityClocksToSystem()
+        cityClocks = getCityClocksToSystem()
+        mutableCityColocks = cityClocks
+        
         setSectionTitles()
         configureTableViewCell()
         configureSearchBar()
@@ -74,12 +78,12 @@ class ChooseACityViewController: UIViewController {
 //            headerTitles.append(UnicodeScalar(num)!.description)
 //        }
         
-        sectionTitles = Array(Set(cityColocks.compactMap{
+        sectionTitles = Array(Set(mutableCityColocks.compactMap{
             cityColockDict[String($0.cityName.prefix(1))] = [CityClock]()
             return String($0.cityName.prefix(1))
         }))
         sectionTitles.sort()
-        cityColocks.forEach{
+        mutableCityColocks.forEach{
             cityColockDict[String($0.cityName.prefix(1))]?.append($0)
             
         }
@@ -88,27 +92,47 @@ class ChooseACityViewController: UIViewController {
 
 extension ChooseACityViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearching {
+            return mutableCityColocks.count
+        }
+        
         return cityColockDict[sectionTitles[section]]!.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ChooseACityTableViewCell.identifier, for: indexPath) as? ChooseACityTableViewCell else {return UITableViewCell()}
         
-        cell.configureView(cityColockDict[sectionTitles[indexPath.section]]?[indexPath.row].cityName ?? "", cityColockDict[sectionTitles[indexPath.section]]?[indexPath.row].countryName ?? ""
-        )
+        if isSearching {
+            cell.configureView(mutableCityColocks[indexPath.row].cityName, mutableCityColocks[indexPath.row].countryName)
+            return cell
+        }
+        
+        cell.configureView(cityColockDict[sectionTitles[indexPath.section]]?[indexPath.row].cityName ?? "", cityColockDict[sectionTitles[indexPath.section]]?[indexPath.row].countryName ?? "")
         
         return cell
     }
     
     func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        if isSearching {
+            return nil
+        }
+        
         return sectionTitles
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if isSearching {
+            return 1
+        }
+        
         return sectionTitles.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if isSearching {
+            return nil
+        }
+        
         return sectionTitles[section]
     }
 }
@@ -116,5 +140,28 @@ extension ChooseACityViewController: UITableViewDataSource {
 extension ChooseACityViewController: UISearchBarDelegate {
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         self.dismiss(animated: true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        isSearching = true
+        
+        if searchText.isEmpty {
+            mutableCityColocks = cityClocks
+            isSearching = false
+            self.cityTableView.reloadData()
+            return
+        }
+        var temp = [CityClock]()
+
+        isSearching = true
+        
+        for city in mutableCityColocks {
+            if city.cityName.uppercased().contains(searchText.uppercased()) {
+                temp.append(city)
+            }
+        }
+        
+        mutableCityColocks = temp
+        self.cityTableView.reloadData()
     }
 }
